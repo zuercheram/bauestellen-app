06083fb158fe3cf94af6405994e9fea1921c8011
zuercheram <amos.zuercher@gmail.com>
2024-10-27 23:03:13 +0100

    Folder structure created and MAUI App client initialized

---
+++ src/Clients/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Clients;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ src/Clients/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Clients;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
