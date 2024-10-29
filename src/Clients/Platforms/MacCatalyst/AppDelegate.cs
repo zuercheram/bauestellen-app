44129f08c87c3c950a9a06485cb44ea0f50dfc68
Amos Zürcher <amos.zuercher@gmail.com>
2024-10-29 20:31:17 +0100

    Repository cleanup performed and code file structure defined

--- src/Clients/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace Clients;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
